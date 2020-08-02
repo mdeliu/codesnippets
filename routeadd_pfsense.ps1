92dc02a0f62e36d6e98255375654b332ecad96d9
mdeliu <64710325+mdeliu@users.noreply.github.com>
2020-08-02 11:35:20 +0200

    Update routeadd_pfsense.ps1

--- routeadd_pfsense.ps1
+++ routeadd_pfsense.ps1
@@ -21,10 +21,11 @@ if ((Check-Admin) -eq $false)
 exit
 }
 
+# set field
 # set the variables for the firewall network (here: LAN1)
-$ip='10.154.30.0'          # Network from pfSense LAN1
+$ip='0.0.0.0'          # Network from pfSense LAN1
 $mask='255.255.255.0'      # Subnet from pfSense LAN1
-$gateway='192.168.178.120' # IP from pfSense WAN 
+$gateway='192.168.178.0' # IP from pfSense WAN 
 
 # add route
 route add $ip mask $mask $gateway
