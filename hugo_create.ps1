13bbbd40c0ca81fe698670f15f221a3b9aa98e64
unknown <musano.deliu@gmx.de>
2021-09-11 15:22:16 +0200

    update

--- hugo_create.ps1
+++ hugo_create.ps1
@@ -1,13 +1,21 @@
+<# 
+Requirements
+- installed chocolatey via Powershell
+- installed Hugo via chocolatey
+- created Hugo site under the $hugopubliccreate directory
+- init git under $hugopublic
+#>
+
 # Paths
-$hugopubliccreate = "D:\Projects\mdeliu.github.io"
-$hugopublic = "D:\Projects\mdeliu.github.io\public"
+$hugopubliccreate = "D:\IT Projects\mdeliu.github.io"   # path to Hugo site
+$hugopublic = "D:\IT Projects\mdeliu.github.io\public"  # path to Hugo public site
 
 # Functions
 
 Function Hugo-Clear {
     Write-Host "changing work directory to" $hugopublic
     Set-Location -Path $hugopublic
-    Write host "clearing public directory" $hugopublic
+    Write-host "clearing public directory" $hugopublic
     Remove-Item -recurse $hugopublic\* -exclude $hugopublic\.git
 }
 
